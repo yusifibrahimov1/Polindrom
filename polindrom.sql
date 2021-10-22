eb506bf1dbdcf67a857145d09a02e13fdfa24d04
Yusif <87138353+yusifibrahimov1@users.noreply.github.com>
2021-10-22 22:23:35 +0400

    Update polindrom.sql

--- polindrom.sql
+++ polindrom.sql
@@ -2,28 +2,35 @@
 
 CREATE OR REPLACE PROCEDURE polindrom (a nvarchar2)
 IS
-  b nvarchar2(10);
-  c nvarchar2(10);
+  b varchar(10);
+  c varchar(10);
   d int;
-  e int := 1;
-  error_polindrom EXCEPTION;
+  e int:= 1;
+  m int;
 BEGIN
   d:= LENGTH(a);
       LOOP
-                b:= SUBSTR(a,e,1);
-                c:= SUBSTR(a,d,1);
-                    IF b<>c THEN
-                       dbms_output.put_line(a||' is not Polindrom');
-                       EXIT;
-                    ELSE
-                       RAISE error_polindrom;
-                    END IF;
-                d:=d-1;
-                    EXIT WHEN d=0;
-                e:=e+1;
-                    EXIT WHEN e=LENGTH(a);
+        b:= SUBSTR(a,d,1);
+        c:= SUBSTR(a,e,1);
+            IF b=c THEN
+               m:=1;
+            ELSIF b<>c THEN
+               m:=0;
+               EXIT;
+            END IF;
+        d:= d-1;
+        e:= e+1;
+        EXIT WHEN d=0;
+        EXIT WHEN e=LENGTH(a);
       END LOOP;
-  EXCEPTION
-      WHEN error_polindrom THEN
-           dbms_output.put_line(a||' is Polindrom');
+  IF m=1 THEN
+        dbms_output.put_line(a||' is Polindrom');
+  ELSIF m=0 THEN
+        dbms_output.put_line(a||' is not Polindrom');
+  END IF;
 END;
+
+/* EXECUTE PROCEDURE */
+
+EXECUTE PROCEDURE polindrom('123321');
+/
